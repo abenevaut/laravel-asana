cabaf9a814d99ef7acc00a7e66a2a29ef6b8e9aa
abenevaut <antoine.benevaut+github@global-exam.com>
2022-09-03 20:09:42 +0000

    Update master to output generated at d2f29da

---
+++ config/asana.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'access_token' => env('ASANA_ACCESS_TOKEN'),
+];
---
+++ src/Factories/AsanaDriverFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace abenevaut\Asana\Factories;
+
+use abenevaut\Asana\Contracts\ApiRepositoryAbstract;
+use abenevaut\Asana\Contracts\AsanaDriversEnum;
+
+class AsanaDriverFactory
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    public function drive(AsanaDriversEnum $driver): ApiRepositoryAbstract
+    {
+        $class = "\abenevaut\Asana\Repositories\{$driver->name}Repository";
+
+        return new $class();
+    }
+}
